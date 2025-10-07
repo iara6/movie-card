a5dd456556477d899828c056942ffc11c4a55879
Irene <grim6ace@gmail.com>
2025-10-08 01:01:52 +0300

    ver 5: upd

--- movie-card.js
+++ movie-card.js
@@ -6,17 +6,18 @@ const modal = document.querySelector('.modal');
 const moviePoster = document.querySelector('.movie-poster img');
 const modalImg = document.getElementById('img1');
 const closeBtn = document.querySelector('.close');
-const movieCard = document.querySelector('.movie-card');
 
 function openModal(src) {
   modalImg.src = src;
   modal.classList.add('open');
-  document.body.classList.add('modal-open')
+  document.body.classList.add('modal-open');
+  setTimeout(() => modalImg.classList.add('modal-open'), 35);
 }
 
 function closeModal() {
   modal.classList.remove('open');
-  document.body.classList.remove('modal-open')
+  document.body.classList.remove('modal-open');
+  modalImg.classList.remove('modal-open');
 }
 
 moviePoster.addEventListener('click', (e) => {
@@ -28,3 +29,10 @@ closeBtn.addEventListener('click', closeModal);
 modal.addEventListener('click', (e) => {
   if (e.target === modal) closeModal();
 });
+
+
+/**************
+ TRAILER MODAL 
+***************/
+
+const watchBtn = document.querySelector('.watch-trailer-button');
